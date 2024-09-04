cdca2051d5c0e0d44433c5f9f20e9f45817ee71d
Olive Daly <olive-branch@posteo.me>
2024-09-04 05:27:54 -0400

    revise styling and integrate with rest API

--- src/App.js
+++ src/App.js
@@ -6,12 +6,60 @@ import Schedule from './pages/Schedule';
 import Contact from './pages/Contact';
 import RadioPlayer from './components/RadioPlayer';
 import Upcoming from './pages/Upcoming';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const style = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    zIndex: -1,
+    background: 'url(/images/offline.jpg) no-repeat center center fixed',
+    backgroundSize: 'cover',
+    filter: 'blur(10px)',
+  };
+  //fetch data from api and store in state every 1 minute
+
+  const fetchData = async () => {
+
+    try {
+      const response = await fetch(
+        'https://m-m-radiostation.onrender.com/nowplaying'
+      );
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const [data, setData] = useState(null);
+  useEffect(() => 
+  {
+    fetchData();
+    const interval = setInterval(() => {
+      fetchData();
+    }, 60000);
+    return () => clearInterval(interval);
+  }
+  ,[]);
+
+  if (data) {
+    if (data.background_img) {
+      style.background = `url(${data.background_img}) no-repeat center center fixed`;
+      style.backgroundSize = 'cover';
+    }
+  }
+
+
   return (
     <div className="container">
     <Router>
-      <RadioPlayer />
+    <div className="background" style={style}></div>
+      <RadioPlayer data={data}/>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/RadioPlayer.js
+++ src/components/RadioPlayer.js
@@ -1,19 +1,60 @@
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import 'react-h5-audio-player/lib/styles.css'; 
+import './RadioPlayer.css';
 
-const RadioPlayer = () => {
-  return (
-    <div id='mmRadioPlayer'>
-      <iframe width="380" height="470" src="https://cdn2.cloudrad.io/mmradio/live/p1.html" frameborder="0"></iframe>
-      <div id='mmRadioPlayerNowPlaying'>
-        <div>
-        <h1>Regular Programming</h1>
-        <p>The M-M Radio playlist curated by all the students as well as The Neighbor</p>
-        </div>
-        </div>
-    </div>
-  );
+const RadioPlayer = ({data}) => {
+
+  const { show_title, show_host, show_description, cover_img } = data || {};
+
+  if (!cover_img || !show_title || !show_description || !show_host) {
+    return (
+      <div id='mmRadioPlayer'>
+          <div id='mmRadioPlayerCover'>
+          <img src='/images/logo.svg' className='cover-art cover-bg' alt='logo'/>
+          <div id = 'mmRadioPlayerControls'>
+            <AudioPlayer
+            autoPlay
+            id='mmRadioPlayerAudio'
+            src='http://15849.cloudrad.io:9114/live'
+            onPlay={e => console.log("onPlay")}
+            />
+            </div>
+          </div>
+          <div id='mmRadioPlayerNowPlaying'>
+          <div>
+          <h1>Offline</h1>
+          <p>View our schedule and future shows below.</p>
+          </div>
+          </div>
+
+      </div>
+    );
+  }
+  else {
+    return (
+      <div id='mmRadioPlayer'>
+          <div id='mmRadioPlayerCover'>
+          <img src={cover_img} alt='cover'/>
+          <div id = 'mmRadioPlayerControls'>
+            <AudioPlayer
+            autoPlay
+            id='mmRadioPlayerAudio'
+            src='http://15849.cloudrad.io:9114/live'
+            onPlay={e => console.log("onPlay")}
+            />
+          </div>
+          </div>
+          <div id='mmRadioPlayerNowPlaying'>
+          <div>
+          <h1>{show_title}</h1>
+          <h2>{show_host}</h2>
+          <p>{show_description}</p>
+          </div>
+          </div>
+      </div>
+    );
+  }
 };
 
 export default RadioPlayer;
